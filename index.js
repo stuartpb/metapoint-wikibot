f4e8b5177d5ad08e8a9bb8ded6809f2c1fc1c449
Stuart P. Bentley <stuart@testtrack4.com>
2012-12-03 07:59:33 -0500

    Refactor to use two repetetetive functions instead of the same basic code several times

--- index.js
+++ index.js
@@ -10,60 +10,80 @@ var password = process.argv[3]
 var resumeFrom = process.argv[4]
 var eilimit = 500
 
+function wpApiPost(params,callback){
+  var req = agent.post(apiurl)
+  req.type('form')
+    .set('User-Agent', uaString)
+    .send('format','json')
+    .send('maxlag','5')
+    .send(params)
+    .end(function(err,res){
+      if(err){
+        console.error(err)
+      //TODO: handle maxlag
+      } else {
+        callback(res)
+      }
+    })
+}
+
+function wpApiGet(params,callback){
+  var req = agent.get(apiurl)
+  req.set('User-Agent', uaString)
+    .query('format','json')
+    .query('maxlag','5')
+    .query(params)
+    .end(function(err,res){
+      if(err){
+        console.error(err)
+      //TODO: handle maxlag
+      } else {
+        callback(res)
+      }
+    })
+}
+
 //Step 1: Log in.
 function step1(){
-  agent.post(apiurl)
-    .type('form')
-    .set('User-Agent', uaString)
-    .send({
+    wpApiPost({
       action: 'login',
       lgusername: username,
       format: 'json'
-    })
-    .end(function(err,res){
-      if(!err && res.login.result == 'NeedToken'){
-        agent.post('http://'+wikihost+'/api.php')
-          .type('form')
-          .set('User-Agent', uaString)
-          .send({
+    },function(res){
+      if(res.login.result != 'NeedToken'){
+        console.error('Initial response was not NeedToken but '+
+          res.login.result)
+      } else {
+        wpApiPost({
             action: 'login',
             lgusername: username,
             lgpassword: password,
             lgtoken: res.body.login.token,
-            format: 'json'
-          })
-          .end(function(err,res){
-            if (err) {
-              console.error(err)
-            } else if (res.login.result != 'Success'){
+          },function(res){
+            if (res.login.result != 'Success'){
               console.error('Login was not Success but '+res.login.result)
             } else {
               getTransclusions(resumeFrom)
             }
           })
-      } else if (err){
-        console.error(err)
-      } else {
-        console.error('Initial response was not NeedToken but '+
-          res.login.result)
       }
     })
 }
 
 //Step 2: Get list of pages transcluding target template.
 function getTransclusions(eicontinue){
-  agent.get(apiurl)
-    .query({
-      action: 'query',
-      list: 'embeddedin',
-      eititle: 'Template:Tv.com',
-      eilimit: eilimit,
-      })
-  if(eicontinue){agent.query('eicontinue',eicontinue)}
-  agent.end(handleTransclusions)
+  var params = {
+    action: 'query',
+    list: 'embeddedin',
+    eititle: 'Template:Tv.com',
+    eilimit: eilimit
+  }
+  if(eicontinue){params.eicontinue = eicontinue}
+  wpApiGet(params,handleTransclusions)
 }
 
-function handleTransculsions(err,res){
+function handleTransclusions(res){
   
 }
+
 step1()
