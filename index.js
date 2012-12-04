e036b26da57ea1ad6cb56b73cc1f259685c2499e
Stuart P. Bentley <stuart@testtrack4.com>
2012-12-03 19:00:54 -0500

    Semicolons!

--- index.js
+++ index.js
@@ -1,58 +1,58 @@
-var agent = require("superagent").agent()
+var agent = require("superagent").agent();
 
 //Step 0: Configure.
 var uaString = 'Metapoint-Wikibot/0.1'
-  + ' (http://github.com/stuartpb/metapoint-wikibot; stuart@testtrack4.com)'
-var apiurl = 'http://en.wikipedia.org/w/api.php'
-var username = process.argv[2]
-var password = process.argv[3]
-var resumeFrom = process.argv[4]
-var eilimit = 500
-var maxlag = 5
+  + ' (http://github.com/stuartpb/metapoint-wikibot; stuart@testtrack4.com)';
+var apiurl = 'http://en.wikipedia.org/w/api.php';
+var username = process.argv[2];
+var password = process.argv[3];
+var resumeFrom = process.argv[4];
+var eilimit = 500;
+var maxlag = 5;
 //Number of page contents to request in one query
-var contentBatchSize = 25
+var contentBatchSize = 25;
 //Maximum number of pages to edit concurrently
-var maxParallelEdits = 10
+var maxParallelEdits = 10;
 
 //Helper functions
 function wpApiCall(method,params,callback){
-  var req
+  var req;
 
   if (method=='get'){
-    req = agent.get(apiurl)
+    req = agent.get(apiurl);
     req.query('format','json')
       .query('maxlag',maxlag)
-      .query(params)
+      .query(params);
   } else if (method=='post') {
-    req = agent.post(apiurl)
+    req = agent.post(apiurl);
   //Technically I don't have to use a form body for a POST,
   //but thousands-of-character-long query strings scare me.
     req.type('form')
       .send('format','json')
       .send('maxlag',maxlag)
-      .send(params)
+      .send(params);
   } else {
-    throw new Error('attempted API call with non-GET/POST method')
+    throw new Error('attempted API call with non-GET/POST method');
   }
   req.set('User-Agent', uaString)
     .end(function(err,res){
       if(err){
-        console.error(err)
+        console.error(err);
       } else if (!res.ok) {
-        console.error('API call returned status ' + res.status)
+        console.error('API call returned status ' + res.status);
       } else if (res.header['mediawiki-api-error']) {
         if(res.header['mediawiki-api-error'] == 'maxlag') {
           setTimeout(wpApiCall,
             res.header['retry-after'] * 1000,
-            method,params,callback)
+            method,params,callback);
         } else {
           console.error('MediaWiki API returned error header '+
-           res.header['mediawiki-api-error'])
+           res.header['mediawiki-api-error']);
         }
       } else {
-        callback(res)
+        callback(res);
       }
-    })
+    });
 }
 
 //Step 1: Log in.
@@ -64,7 +64,7 @@ function step1(){
     },function(res){
       if(res.login.result != 'NeedToken'){
         console.error('Initial response was not NeedToken but '+
-          res.login.result)
+          res.login.result);
       } else {
         wpApiCall('post',{
             action: 'login',
@@ -73,27 +73,27 @@ function step1(){
             lgtoken: res.body.login.token,
           },function(res){
             if (res.login.result != 'Success'){
-              console.error('Login was not Success but '+res.login.result)
+              console.error('Login was not Success but '+res.login.result);
             } else {
-              getEditToken()
+              getEditToken();
             }
-          })
+          });
       }
-    })
+    });
 }
 
 //Step 1.5: Get edit token.
-var editToken
+var editToken;
 function getEditToken(){
   wpApiCall('get', {
     action: 'tokens',
     type: 'edit'
   }, function(res) {
     if (res.body.token) {
-      editToken = res.body.token.edittoken
-      getTransclusions(resumeFrom)
+      editToken = res.body.token.edittoken;
+      getTransclusions(resumeFrom);
     }
-  })
+  });
 }
 
 //Step 2: Get list of pages transcluding target template.
@@ -104,26 +104,26 @@ function getTransclusions(eicontinue){
     eititle: 'Template:Tv.com',
     einamespace: 0, //let's not fix non-article usages, at least not right now
     eilimit: eilimit
-  }
+  };
   if(eicontinue){params.eicontinue = eicontinue}
-  wpApiCall('get',params,handleTransclusions)
+  wpApiCall('get',params,handleTransclusions);
 }
 
 function handleTransclusions(res){
-  var pageList = res.body.query.embeddedin
-  var topPage = 0
+  var pageList = res.body.query.embeddedin;
+  var topPage = 0;
   function getContentBatch(){
     if(topPage < pageList.length) {
       var pageids = pageList.slice(topPage, topPage + contentBatchSize)
-        .map(function(ei){return ei.pageid})
-        getPageContents(pageids,getContentBatch)
+        .map(function(ei){return ei.pageid});
+        getPageContents(pageids,getContentBatch);
     } else if (res.body['query-continue']) {
-      getTransclusions(res.body['query-continue'].eicontinue)
+      getTransclusions(res.body['query-continue'].eicontinue);
     }
   }
 }
 
-var runningEdits = 0
+var runningEdits = 0;
 function getPageContents(ids,endCb){
   wpApiCall('get',{
     action: 'query',
@@ -133,26 +133,26 @@ function getPageContents(ids,endCb){
   },function(res){
     
     //The index of the next page to update.
-    var topPage = 0
+    var topPage = 0;
     
     //Spawn new edit (or request more contents.)
     function spawnNewEdit(){
       if (topPage < ids.length) {
-        updatePage(res.body.pages[ids[topPage]],spawnNewEdit)
-        ++topPage
+        updatePage(res.body.pages[ids[topPage]],spawnNewEdit);
+        ++topPage;
       } else {
         //All pages updated, call the end callback (request more contents)
-        endCb()
+        endCb();
       }
     }
     while(runningEdits < maxParallelEdits){
-      spawnNewEdit()
+      spawnNewEdit();
     }
-  })
+  });
 }
 
 function updatePage(page,endCb){
   
 }
 
-step1()
+step1();
